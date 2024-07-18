8a8e09b5a4230be90f994637bd4e774fa622c610
Jyoshna-15 <117263367+Jyoshna-15@users.noreply.github.com>
2024-07-18 21:07:02 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,51 @@
+
+//Ess-functionally scoped(var)
+//es6-block scoped (list const)
+// var abc=10;
+// function testing(){
+//     var abc=20;
+//     let def=100;
+//     console.log(abc);
+//     //block space
+//     if(1){
+//         var xyz='block scooped';
+//         console.log('Inside if',xyz);
+//         console.log(abc);
+//     }
+//     console.log("outside if",xyz);
+// }
+// testing();
+
+
+// var name="Jyoshna";
+// let year=2024;
+// function testing(){
+//     console.log(name);
+//     console.log(year);
+
+// }
+// testing();
+
+
+//function declaration and function expression
+
+// myFunc();
+// myFunc2();
+// function myFunc(){
+//     console.log("Inside the declaration");
+
+// }
+// let myFunc2=function(){
+//     console.log("Inside myFunc2:function expression");
+// }
+// myFunc();
+// myFunc2();
+
+
+console.log(name2)
+name1();
+
+var name2="shaa";
+var name1=function(){
+    console.log('hello');
+}
